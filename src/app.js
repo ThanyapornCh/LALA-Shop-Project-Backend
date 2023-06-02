82b441bd57dd2c7ee455e483f47e3c07c1034cfa
DESKTOP-6S97JAT\Nero <thanyaporn.ch688@gmail.com>
2023-06-02 15:09:27 +0700

    start commit initial

---
+++ src/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(
+  rateLimit({
+    windowMs: 1000 * 60 * 15,
+    max: 1000,
+    message: { message: 'too many requests, please try again later' },
+  })
+);
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 8000;
+app.listen(port, () =>
+  console.log(chalk.yellowBright.italic.bold(`server running on port:${port}`))
+);
