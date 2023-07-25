0837698de7fe3ee1046b758e7deacd7c53d5db97
DESKTOP-6S97JAT\Nero <thanyaporn.ch688@gmail.com>
2023-07-25 13:41:41 +0700

    operation status admin and user

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -12,17 +12,37 @@ module.exports = async (req, res, next) => {
     const token = authorization.split(' ')[1];
     const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
     const user = await User.findOne({
-      where: { id: payload.id },
+      where: { id: payload.id, status: payload.status },
       attributes: {
         exclude: ['password'],
       },
     });
-    if (!user) {
+    const admin = await User.findOne({
+      where: { id: payload.id, status: payload.status },
+      attributes: {
+        exclude: ['password'],
+      },
+    });
+    console.log(payload.status);
+
+    console.log(user.status);
+
+    if (user) {
+      if (user.status === 'user') {
+        req.user = user;
+        next();
+      } else if (user.status === 'admin') {
+        req.user = admin;
+        next();
+      }
+    } else {
       createError('you are unauthorized', 400);
     }
-
-    req.user = user;
-    next();
+    // if (!user) {
+    //   createError('you are unauthorized', 400);
+    // }
+    // req.user = user;
+    // next();
   } catch (err) {
     next(err);
   }
